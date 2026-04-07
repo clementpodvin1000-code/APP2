28b1d4fe4c0d0b1fa1c059667a1ebc7e9aa01dab
clementpodvin1000-code <clementpodvin1000@gmail.com>
2026-04-07 11:27:31 +0200

    Update code_clemsou.py

--- code_clemsou.py
+++ code_clemsou.py
@@ -46,3 +46,13 @@ def selection_sort(L):
     print("Nombre de comparaisons:", w)
     return L        
 print("tri selection:" + str(selection_sort([5, 2, 8, 1, 9, 3, 7, 4, 6, 0, 12, 11, 10, 15, 14, 13, 20, 19, 18, 17, 16, 25, 24, 23, 22, 21, 30, 29, 28, 27, 26, 35, 34, 33, 32, 31, 40, 39, 38, 37, 36, 45, 44, 43, 42, 41, 50, 49, 48, 47, 46])))
+
+
+def convertir_en_liste(AVIONS_INITIAL,parametre):
+    n=len(AVIONS_INITIAL)
+    for i in range(n):
+        AVIONS_INITIAL[i]=AVIONS_INITIAL[i][parametre]
+
+
+
+
